1fa73a3b5f0e8404d6c7a326cdfe7e9b474309f7
Abderahim Ouhamou <aniffour.dev@gmail.com>
2024-11-19 12:09:07 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import "animate.css";
 import Navbar from "./components/Global/Header/Navbar";
