65ffa3e04c136210f8aa3ef2b24354144214d166
Abderahim Ouhamou <aniffour.dev@gmail.com>
2024-11-22 21:24:14 +0100

    Update page.tsx

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -4,13 +4,11 @@ import { getContentBySlug, getAllSlugs } from "@/apis/graphql/content";
 import { Metadata } from "next";
 import Article from "@/app/components/Dynamic/Contents/Article";
 import Page from "@/app/components/Dynamic/Contents/Page";
-// import Category from "@/app/components/Dynamic/Contents/Category";
 
 type Props = {
   params: { slug: string[] };
 };
 
-// Define a type for Post
 interface Post {
   title: string;
   slug: string;
@@ -32,7 +30,7 @@ async function fetchContent(slug: string[]) {
   return content;
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata({ params }: { params: { slug: string[] } }): Promise<Metadata> {
   const content = await fetchContent(params.slug);
   if (!content) {
     return {
@@ -68,48 +66,10 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
         },
       };
     case "page":
-      return {
-        title: content.title,
-        description: content.seo?.metaDesc || description,
-        robots: {
-          index: true,
-          follow: true,
-          googleBot: {
-            index: true,
-            follow: true,
-            "max-video-preview": -1,
-            "max-image-preview": "large",
-            "max-snippet": -1,
-          },
-        },
-        alternates: {
-          canonical: canonicalUrl,
-        },
-      };
     case "post":
       return {
         title: content.title,
         description: content.seo?.metaDesc || description,
-        openGraph: {
-          title: content.seo?.title || content.title,
-          description: content.seo?.metaDesc || description,
-          type: "article",
-          publishedTime: content.seo?.opengraphPublishedTime,
-          modifiedTime: content.seo?.opengraphModifiedTime,
-          authors: [content.author?.node?.name],
-          images: [
-            {
-              url: content.featuredImage?.node?.sourceUrl || "",
-              alt: content.featuredImage?.node?.altText || "",
-            },
-          ],
-        },
-        twitter: {
-          card: "summary_large_image",
-          title: content.seo?.title || content.title,
-          description: content.seo?.metaDesc || description,
-          images: [content.featuredImage?.node?.sourceUrl || ""],
-        },
         robots: {
           index: true,
           follow: true,
@@ -136,66 +96,12 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   }
 }
 
-export default async function DynamicPage({ params }: Props) {
+export default async function DynamicPage({ params }: { params: { slug: string[] } }) {
   const content = await fetchContent(params.slug);
   if (!content) notFound();
 
-  // Schema Markup
-  let schemaMarkup = {};
+  const schemaMarkup = getSchemaMarkup(content);
 
-  switch (content.type) {
-    case "post":
-      schemaMarkup = {
-        "@context": "https://schema.org",
-        "@type": "Article",
-        headline: content.title,
-        description: content.seo?.metaDesc || content.description,
-        image: content.featuredImage?.node?.sourceUrl || "",
-        author: {
-          "@type": "Person",
-          name: content.author?.node?.name,
-        },
-        datePublished: content.seo?.opengraphPublishedTime,
-        dateModified: content.seo?.opengraphModifiedTime,
-      };
-      break;
-    case "page":
-      schemaMarkup = {
-        "@context": "https://schema.org",
-        "@type": "WebPage",
-        name: content.title,
-        description: content.description,
-      };
-      break;
-    case "category":
-    case "subCategory":
-      schemaMarkup = {
-        "@context": "https://schema.org",
-        "@type": "ItemList",
-        name: content.name,
-        itemListElement: Array.isArray(content.posts)
-          ? content.posts.map((post: Post, index: number) => ({
-              "@type": "ListItem",
-              position: index + 1,
-              item: {
-                "@type": "BlogPosting",
-                name: post.title,
-                url: `/${post.slug}`,
-              },
-            }))
-          : [],
-      };
-      break;
-    default:
-      schemaMarkup = {
-        "@context": "https://schema.org",
-        "@type": "WebPage",
-        name: "Dynamic Content Page",
-      };
-      break;
-  }
-
-  // Stringify the schema markup
   const jsonLd = JSON.stringify(schemaMarkup);
 
   return (
@@ -218,4 +124,53 @@ export default async function DynamicPage({ params }: Props) {
 export async function generateStaticParams() {
   const slugs = await getAllSlugs();
   return slugs.map((slug) => ({ slug: slug.split("/") }));
+}
+
+function getSchemaMarkup(content: any) {
+  switch (content.type) {
+    case "post":
+      return {
+        "@context": "https://schema.org",
+        "@type": "Article",
+        headline: content.title,
+        description: content.seo?.metaDesc || content.description,
+        image: content.featuredImage?.node?.sourceUrl || "",
+        author: {
+          "@type": "Person",
+          name: content.author?.node?.name,
+        },
+        datePublished: content.seo?.opengraphPublishedTime,
+        dateModified: content.seo?.opengraphModifiedTime,
+      };
+    case "page":
+      return {
+        "@context": "https://schema.org",
+        "@type": "WebPage",
+        name: content.title,
+        description: content.description,
+      };
+    case "category":
+      return {
+        "@context": "https://schema.org",
+        "@type": "ItemList",
+        name: content.name,
+        itemListElement: Array.isArray(content.posts)
+          ? content.posts.map((post: Post, index: number) => ({
+              "@type": "ListItem",
+              position: index + 1,
+              item: {
+                "@type": "BlogPosting",
+                name: post.title,
+                url: `/${post.slug}`,
+              },
+            }))
+          : [],
+      };
+    default:
+      return {
+        "@context": "https://schema.org",
+        "@type": "WebPage",
+        name: "Dynamic Content Page",
+      };
+  }
 }
